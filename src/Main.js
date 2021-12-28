6df93be4dcbe205e7ae874f2d8ea1d73a8dd34cd
shawshank37013 <mshaw@pivottechschool.com>
2021-12-28 15:34:50 -0600

    added stuf to fix errors

--- src/Main.js
+++ src/Main.js
@@ -3,15 +3,19 @@ import { Container, Row, Col } from "react-bootstrap";
 import CitySearch from "./CitySearch";
 import axios from "axios";
 import LatLon from "./LatLon";
+import Map from './Map';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      latitude: "",
       location: "",
+      latitude: "",
       longitude: "",
       searchQuery: "",
+      displayMap: false,
+      displayError: false,
+      errorMessage: '',
     };
   }
 
@@ -22,12 +26,22 @@ class Main extends React.Component {
   displayLatLon = async () => {
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MAP_KEY}&q=${this.state.searchQuery}&format=json`;
     let location;
+    try{
     location = await axios.get(url);
     this.setState({
       location: location.data[0].display_name,
       latitude: location.data[0].lat,
       longitude: location.data[0].lon,
+      displayMap: true,
+      displayError: false
     });
+    } catch(error){
+      this.setState({
+        displayMap: false,
+        displayError: true,
+        errorMessage: error.response.status + ': ' + error.response.data.error
+      });
+    }
   };
 
   render() {
@@ -38,10 +52,12 @@ class Main extends React.Component {
             <CitySearch
               updateCity={this.updateCity}
               displayLatLon={this.displayLatLon}
+              hasError={this.state.displayError}
+              errorMessage= {this.state.errorMessage}
             />
           </Col>
         </Row>
-        {this.state.location && (
+        {this.state.displayMap && (
           <>
             <Row>
               <Col>
@@ -49,6 +65,14 @@ class Main extends React.Component {
                   city={this.state.location}
                   lat={this.state.latitude}
                   lon={this.state.longitude}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Map
+                  img_url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_MAP_KEY}&center=${this.state.latitude},${this.state.longitude}&size=${window.innerWidth}x300&format=jpg&zoom=12`}
+                  city={this.state.location}
                 />
               </Col>
             </Row>
